1794a5e74ec2943e0ff20e4ca5256d1e19ebca6c
Jurzo <juuso.kalliomaki96@gmail.com>
2021-10-14 13:34:41 +0300

    fussyroute

--- src/Data/ItineraryData.js
+++ src/Data/ItineraryData.js
@@ -3,7 +3,10 @@ const url = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
 export const fetchDuration = async (from, destination) => {
     let duration = -1;
     const to = await fetchStop(destination);
-    if (to === undefined) return undefined;
+    if (to === undefined) {
+        console.log('undefined destination');
+        return undefined;
+    }
     duration = await fetchItinerary(from, to)
     //console.log('duration', duration);
     return duration;
@@ -54,6 +57,41 @@ export const fetchItinerary = async (from, to) => {
     }
 }
 
+/**
+ * Query end stop id from bus data
+ * @param {{route: string, direction: number, date: string, time: number}} data
+ * @returns {Promise<string>}
+ */
+export const fuzzyTripQuery = async (data) => {
+    let id;
+    const { route, direction, date, time } = data;
+    const requestBody = `{
+    fuzzyTrip(route: "${route}", direction:${direction}, date: "${date}", time: ${time}) {
+        route {
+        shortName
+        }
+        pattern {
+        name
+        }
+    }
+    }`;
+    const req = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/graphql' },
+        body: requestBody
+    }
+    let resp;
+    try {
+        resp = await fetch(url, req);
+    } catch (err) {
+        console.error('Failed to fetch fuzzyroute', err);
+        return undefined;
+    }
+    const json = await resp.json();
+    if (!json.data.fuzzyTrip) console.log(requestBody);
+    //console.log(json.data.fuzzyTrip);
+}
+
 // hakee päätepysäkin noin suurinpiirtein graphqlstä topicista tuodulla päätepysäkillä
 export const fetchStop = async (destination) => {
     let to;
@@ -79,6 +117,9 @@ export const fetchStop = async (destination) => {
         return undefined;
     }
     const json = await resp.json();
+    /* if (json.data.stops.length < 1) {
+        console.log(destination);
+    } */
     for (let i = 0; i < json.data.stops.length; i++) {
         if (json.data.stops[i].name === destination) {
             to = { lat: json.data.stops[i].lat, long: json.data.stops[i].lon }
