5d227c0b028328c7918893f53e6372b29774d1db
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-10-25 12:43:01 +0300

    menu button class

--- src/Components/MenuList.js
+++ src/Components/MenuList.js
@@ -5,20 +5,16 @@ import { Link } from "react-router-dom";
 export default function MenuList() {
   return (
     <div>
-      <Link className="text-link" to="/">
-        <button>Menu</button>
-      </Link>
-      <br />
       <Link className="text-link" to="/Game">
-        <button>Game</button>
+        <button className="menuButton">Game</button>
       </Link>{' '}
       <br />
       <Link className="text-link" to="/Map">
-        <button>Bus Map</button>
+        <button className="menuButton">Bus Map</button>
       </Link>{' '}
       <br />
       <Link className="text-link" to="/Credits">
-        <button>Credits</button>
+        <button className="menuButton">Credits</button>
       </Link>{''}
       <br />
       <PlaySound />
--- src/Components/PlaySound.js
+++ src/Components/PlaySound.js
@@ -4,20 +4,20 @@ import Running from '../music/run.mp3';
 
 
 const PlaySound = () => {
-     const [isPlaying, setIsPlaying] = useState(false);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     return (
         <div>
-        <button onClick={() => setIsPlaying(!isPlaying)}>{!isPlaying ? 'Play music' : 'Stop music'} </button>
-        <Sound
-        url={Running}
-        playStatus={
-            isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED
-        }
-        playFromPosition={300}
-        loop={true}
-        volume = {20}
-        />
+            <button className="menuButton" onClick={() => setIsPlaying(!isPlaying)}>{!isPlaying ? 'Play music' : 'Stop music'} </button>
+            <Sound
+                url={Running}
+                playStatus={
+                    isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED
+                }
+                playFromPosition={300}
+                loop={true}
+                volume={20}
+            />
         </div>
     );
 };
