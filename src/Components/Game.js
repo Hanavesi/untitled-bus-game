ae09ceb7e1371f4e0e291efb86443768de0e5404
Jurzo <juuso.kalliomaki96@gmail.com>
2021-11-15 13:44:40 +0200

    music no longer gets layered

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,55 +1,57 @@
 import React, { useRef, useState } from 'react';
 import { Engine } from '../Engine/Engine';
-import {Howl, Howler} from 'howler'
+import { Howl, Howler } from 'howler'
 import Running from '../music/run.mp3';
 
 export function Game({ mqttHandler }) {
-    const [ready, setReady] = useState(false);
-    const engine = useRef(null);
-    const visible = { visibility: 'visible' };
-    const hidden = { visibility: 'hidden' };
+  const [ready, setReady] = useState(false);
+  const engine = useRef(null);
+  const visible = { visibility: 'visible' };
+  const hidden = { visibility: 'hidden' };
+
+  React.useEffect(() => {
+    const canvas = document.getElementById("gameCanvas");
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    engine.current = new Engine(canvas, width, height, setReady);
+
+    //mqttHandler.setMessageCallback(onMessage)
+
+    window.addEventListener('resize', () => {
+      const canvas = document.getElementById("gameCanvas");
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      engine.current.onWindowResize(width, height);
+    });
+    window.addEventListener('mousemove', (e) => {
+      const pos = { x: (e.clientX / window.innerWidth) * 2 - 1, y: -(e.clientY / window.innerHeight) * 2 + 1 };
+      engine.current.setMousePos(pos);
+    });
+
     const sound = new Howl({
-        src: [Running],
-        volume: 0.05,
-        loop: true,
-        autoplay: true,
-      });
-      
+      src: [Running],
+      volume: 0.05,
+      loop: true
+    });
+    sound.play();
 
-    React.useEffect(() => {
-        const canvas = document.getElementById("gameCanvas");
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        engine.current = new Engine(canvas, width, height, setReady);
+    return (() => {
+      mqttHandler.disconnect();
+      sound.stop();
+      sound.unload();
+    });
+  }, []);
 
-        //mqttHandler.setMessageCallback(onMessage)
+  const onMessage = (message, topic) => {
+    // do stufs
+    console.log(message);
+  }
 
-        window.addEventListener('resize', () => {
-            const canvas = document.getElementById("gameCanvas");
-            const width = canvas.clientWidth;
-            const height = canvas.clientHeight;
-            engine.current.onWindowResize(width, height);
-        });
-        window.addEventListener('mousemove', (e) => {
-            const pos = {x:(e.clientX / window.innerWidth) * 2 - 1, y:-(e.clientY / window.innerHeight) * 2 + 1};
-            engine.current.setMousePos(pos);
-        });
-        
-        return(() => {
-            mqttHandler.disconnect();
-        });
-    }, []);
-
-    const onMessage = (message, topic) => {
-        // do stufs
-        console.log(message);
-    }
-
-    return (
-        <div>
-            <canvas id="gameCanvas" style={ready ? visible : hidden} />
-        </div>
-    );
+  return (
+    <div>
+      <canvas id="gameCanvas" style={ready ? visible : hidden} />
+    </div>
+  );
 
 }
 
