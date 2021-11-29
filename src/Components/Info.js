a90e6dbe94e53056f68caeab68c09dfa4a9cce19
jontsaaw <64011152+jontsaaw@users.noreply.github.com>
2021-11-29 14:28:14 +0200

    fixed typo

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -11,7 +11,7 @@ export default function Info() {
       fontSize: 25,
       width: "30%"
     }}>
-      It's year 2021. Supermegavirus has spread all over the world. Virus transforms humans red and hostile. Buses are the only place where you are safe from virus. Protect the bus from enemies and survive to the final stop. Get on to the bus and fight!
+      It's year 2021. Supermegavirus has spread all over the world. The virus transforms humans red and hostile. Buses are the only place where you are safe from the virus. Protect the bus from enemies and survive to the final stop. Get on to the bus and fight!
     </div>
   );
 }
