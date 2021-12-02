c43bfca47b5593b3c9fab3bcafed26d9afc567b2
Niko Lindgren <bgm055@myy.haaga-helia.fi>
2021-12-02 13:19:27 +0200

    changes to score styles

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -80,8 +80,9 @@ export function Game({ mqttHandler }) {
 
   return (
     <div id="gameContainer">
-      <canvas id="gameCanvas" style={showGame ? visible : hidden} />
-      <GameStats score={score} style={showGame ? visible : hidden} />
+      <canvas id="gameCanvas" style={showGame ? visible : visible} />
+      <GameStats score={score} style={showGame ? visible : visible} />
+     
       {
         ready ?
           <div>
--- src/Components/GameStats.js
+++ src/Components/GameStats.js
@@ -11,15 +11,22 @@ export default function GameStats(props) {
   return (
     <div
       style={{
+        fontSize: '25px',
         display: 'flex',
         position: 'absolute',
+        fontFamily: 'Games',
         bottom: 0,
         left: 0,
         right: 0,
         width: '10vw',
+        minWidth: '200px',
+        height: '65px',
         margin: 'auto',
         zIndex: 2,
-        backgroundColor: 'white',
+        backgroundColor: 'black',
+        color: 'white', //this is the text color dummie
+        borderTopLeftRadius: '40px', //makes the round effect
+        borderTopRightRadius: '40px', //makes the round effect
         ...props.style
       }}>
       <p style={{ flex: 3, textAlign: 'center' }}>Score</p>
