c4e8d0a8e759bd8933fd3324e17f937ee30c498c
jontsaaw <64011152+jontsaaw@users.noreply.github.com>
2021-12-06 12:27:30 +0200

    fixed sounds

--- src/Engine/ECS/Systems.js
+++ src/Engine/ECS/Systems.js
@@ -99,8 +99,11 @@ export class HealthSystem extends System {
         }
       };
       if (entity.hasComponent(Playable)) {
-        if (scale < 0.3)
+        if (scale < 0.3){
           !this.world.sounds.isPlaying('lowHp') && this.world.sounds.playSound('lowHp');
+          this.world.sounds.stopSound('bus');
+          this.world.sounds.stopSound('shop');
+        }
         else
           this.world.sounds.stopSound('lowHp');
       }
